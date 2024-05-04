a23e128d20d8e0c96def8de7f191f4131cc06f6d
aliakbarazmoudeh <aliakbarazmoudeh@gmail.com>
2024-05-04 22:27:51 +0430

    minor bug fixed for payments service

--- ads-service/controllers/adsController.js
+++ ads-service/controllers/adsController.js
@@ -2,6 +2,8 @@ const Ad = require('../models/Ad');
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
 const { BadRequestError, NotFoundError } = require('../errors');
+const createPayment = require('../commen/zarinpal/createPayment');
+const verifyPayment = require('../commen/zarinpal/verifyPayment');
 
 const getAllAds = async (req, res) => {
   const ads = await Ad.findAll({
@@ -37,21 +39,55 @@ const createAd = async (req, res) => {
     throw new BadRequestError('Please upload image smaller 0.5MB');
   }
   const imagePath = path.join(__dirname, '../../ads/' + `${productImage.name}`);
-  console.log(imagePath);
-  await productImage.mv(imagePath);
+  // await productImage.mv(imagePath);
   const ad = await Ad.create({
     owner: req.user,
     src: `/ads/${productImage.name}`,
     plan: parseInt(req.query.plan),
     expireAt: Date.now() + 259200000,
   });
+  let amount;
+  ad.dataValues.plan === 1 ? (amount = 49000) : null;
+  ad.dataValues.plan === 3 ? (amount = 89000) : null;
+  ad.dataValues.plan === 5 ? (amount = 109000) : null;
+  const payment = await createPayment(
+    amount,
+    ad.dataValues.owner,
+    `http://localhost:${process.env.PORT || 5003}/api/ad/payments?productID=${
+      ad.dataValues._id
+    }&Amount=${amount}`
+  );
+  console.log(payment);
   res
     .status(StatusCodes.OK)
-    .json({ image: { src: `/ads/${productImage.name}` } });
+    .json({ image: { src: `/ads/${productImage.name}` }, url: payment.url });
+};
+
+const payment = async (req, res) => {
+  const { productID, Amount, Authority, Status } = req.query;
+  const product = await Ad.findOne({ where: { _id: productID } });
+  if (Status != 'OK') {
+    await product.destroy();
+    throw new BadRequestError('invalid payment, pleas try again');
+  }
+  const paymentInfo = await verifyPayment(Amount, Authority);
+  if (
+    !paymentInfo.RefID ||
+    (paymentInfo.status < 100 && paymentInfo.status > 101)
+  ) {
+    await product.destroy();
+    throw new BadRequestError('invalid payments, pleas try again later');
+  }
+  product.set({ payment: true });
+  product.save();
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: 'payment was successfull, congratulation!' });
 };
 
 module.exports = {
   getAllAds,
   getAd,
   createAd,
+  payment,
 };
--- ads-service/routes/adsRoutes.js
+++ ads-service/routes/adsRoutes.js
@@ -4,13 +4,13 @@ const {
   getAllAds,
   createAd,
   getAd,
+  payment,
 } = require('../controllers/adsController');
 const { authenticateUser } = require('../middleware/authentication');
 
-router
-  .route('/api/ads')
-  .get(getAllAds)
-  .post(authenticateUser, createAd)
+router.route('/api/ads').get(getAllAds).post(authenticateUser, createAd);
+
+router.use('/api/ad/payments', payment);
 
 router.route('/api/ad/:id').get(getAd);
 module.exports = router;
